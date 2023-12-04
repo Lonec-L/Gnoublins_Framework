a1b3b7c3f903b0b847f13c98dc121cf0194bebbc
Luka Lonec <luka.lonec@gmail.com>
2023-12-04 17:00:05 +0100

    demo random speed
    gets random speed every second nad displays it on speedometer

--- main.js
+++ main.js
@@ -7,6 +7,26 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 var mtlLoader = new MTLLoader();
 
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function addEvent(element, eventName, callback) {
+    if (element.addEventListener) {
+        element.addEventListener(eventName, callback, false);
+    } else if (element.attachEvent) {
+        element.attachEvent("on" + eventName, callback);
+    } else {
+        element["on" + eventName] = callback;
+    }
+}
+
 mtlLoader.load("models/armaturka/armaturka.mtl", function (materials) {
 
     materials.preload();
@@ -16,6 +36,17 @@ mtlLoader.load("models/armaturka/armaturka.mtl", function (materials) {
     objLoader.load(
         'models/armaturka/armaturka.obj',
         function (object) {
+
+            object.getRandomSpeed = function () {
+                httpGetAsync("http://www.randomnumberapi.com/api/v1.0/random?min=10&max=140&count=1", function (response) {
+                    var value = JSON.parse(response)[0];
+                    object.speed = value;
+                    console.log(value);
+                });
+                setTimeout(object.getRandomSpeed, 1000);
+            }
+            object.getRandomSpeed();
+
             object.rotation.y = Math.PI;
             object.position.z = 4.5;
             console.log(object);
@@ -24,12 +55,25 @@ mtlLoader.load("models/armaturka/armaturka.mtl", function (materials) {
                     //object.children[i].setPosition(0.198229, 0.086486, -0.045737);
                     //object.children[i].position = new THREE.Vector3(0.198229, 0.086486, -0.045737);
                     object.kazalec_1_pos = new THREE.Vector3(0.198229, -0.045737, 0.086486);
-                    object.update = function () {
-                        object.children[i].position.sub(object.kazalec_1_pos);
-                        object.children[i].position.applyAxisAngle(new THREE.Vector3(0, 0, 1), 0.01);
-                        object.children[i].position.add(object.kazalec_1_pos);
 
-                        object.children[i].rotateOnAxis(new THREE.Vector3(0, 0, 1), 0.01);
+                    object.children[i].rot_value = 0;
+                    object.children[i].rot_max = 2.30;
+                    object.children[i].rot_min = -2.30;
+                    object.speed = 0;
+                    object.target_rotation = 0 - 2.30;
+
+                    object.update = function () {
+                        if (object.speed > 10 && object.speed < 140) {
+                            object.target_rotation = ((object.speed - 10) / 130) * 4.60 - 2.30;
+                        }
+                        if (Math.abs(object.target_rotation - object.children[i].rotation.z) > 0.001) {
+                            var rotateBy = (object.target_rotation - object.children[i].rotation.z) / 30;
+                            object.children[i].position.sub(object.kazalec_1_pos);
+                            object.children[i].position.applyAxisAngle(new THREE.Vector3(0, 0, 1), rotateBy);
+                            object.children[i].position.add(object.kazalec_1_pos);
+
+                            object.children[i].rotateOnAxis(new THREE.Vector3(0, 0, 1), rotateBy);
+                        }
                     }
                 }
                 else if (object.children[i].name === "Lučka_gradbišče") {
@@ -60,11 +104,11 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(0, 1, 1); //default; light shining from top
+light.position.set(0, 5, 5); //default; light shining from top
 light.castShadow = true; // default false
 scene.add(light);
 
-const alight = new THREE.AmbientLight(0x404040); // soft white light
+const alight = new THREE.AmbientLight(0x808080); // soft white light
 scene.add(alight);
 
 
@@ -86,11 +130,15 @@ cube.onClicked = function () {
     this.material.color.set(0xff0000);
 }
 
-const planeGeometry = new THREE.BoxGeometry(4, 0.1, 4);
+const planeGeometry = new THREE.BoxGeometry(10, 0.1, 10);
 const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff })
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.position.y = -1;
 plane.receiveShadow = true;
+
+plane.onClicked = function () {
+    this.material.color.set(0xff0000);
+}
 
 scene.add(plane);
 
@@ -116,11 +164,8 @@ function onDocumentMouseDown(e) {
 
     console.log(intersects);
 
-    for (let i = 0; i < intersects.length; i++) {
-
-        intersects[i].object.onClicked();
-
-    }
+    if (intersects[0].object.onClicked)
+        intersects[0].object.onClicked();
 }
 
 document.addEventListener('mousedown', onDocumentMouseDown, false);
